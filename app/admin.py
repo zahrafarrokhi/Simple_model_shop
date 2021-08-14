d4840fb2a1a2f4021343ef2647fec531b16e43ed
zahra_f <73747335+zahrafarrokhi@users.noreply.github.com>
2021-08-14 15:39:13 +0430

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Product,Cart,CartItem
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(CartItem)
+# admin.site.register(Cart)
+# admin.site.register(CartItem)
+
+class CartItemInline(admin.TabularInline):
+	model = CartItem
+	raw_id_fields = ('product',)
+
+
+class CartAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'payment_date ', 'is_paid ')
+	list_filter = ('is_paid',)
+	inlines = (CartItemInline,)
+
+admin.site.register(Cart,OrderAdmin)
+
